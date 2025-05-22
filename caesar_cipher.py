4e7f936ed4d66fb47d5d0c5f1fc3eb6595379d1d
Chistooz <chistooz21@gmail.com>
2025-05-22 09:37:52 +0300

    Начал новый проект

---
+++ caesar_cipher.py
@@ -0,0 +1,14 @@
+eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+symbol = [" ", ",", ".", "!", "?"]
+
+print("Выберите язык: aнгл=e, рус=r")
+lan = input()
+print("Выберите шифрование: шифрование - ch, дешифрование - def")
+chif = input()
+print("Введите ключ шифрования")
+n = int(input())
+print("Введите фразу")
+f = input()
