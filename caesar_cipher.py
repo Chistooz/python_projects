720e0d3ec7aaffa581728cdc4ac31d1d44910c88
Chistooz <chistooz21@gmail.com>
2025-05-22 09:45:14 +0300

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -12,3 +12,38 @@ print("Введите ключ шифрования")
 n = int(input())
 print("Введите фразу")
 f = input()
+
+def chru(chifr, n, l, fraza):
+    if l == 'r':
+        moch = 32
+    if l == 'e':
+        moch = 26
+    if chifr== 'def':
+        n = -n
+    for i in range(len(fraza)):
+        if fraza[i].isalpha():
+            if fraza[i] == fraza[i].upper():
+                for j in range (moch):
+                    if moch == 32:
+                        if fraza[i] == rus_upper_alphabet[j]:
+                            print(rus_upper_alphabet[(j+n)%moch], end = '')
+                            break
+                    if moch == 26:
+                        if fraza[i] == eng_upper_alphabet[j]:
+                            print(eng_upper_alphabet[(j+n)%moch], end = '')
+                            break
+            elif fraza[i] ==fraza[i].lower():
+                for j in range (moch):
+                    if moch == 32:
+                        if fraza[i] == rus_lower_alphabet[j]:
+                           print(rus_lower_alphabet[(j+n)%moch], end='')
+                           break
+                    if moch == 26:
+                        if fraza[i] == eng_lower_alphabet[j]:
+                           print(eng_lower_alphabet[(j+n)%moch], end = '')
+                           break
+        else:
+            print(fraza[i], end='')
+
+chru(chif, n, lan, f)
+
